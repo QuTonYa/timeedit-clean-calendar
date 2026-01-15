867fb95570b7cf7df0ab01357c02cc5f88766c7c
QuTonYa <103885599+QuTonYa@users.noreply.github.com>
2026-01-15 17:01:57 +0100

    Update clean_timeedit.py

--- clean_timeedit.py
+++ clean_timeedit.py
@@ -1,27 +1,31 @@
-import request
-from ics import Calender, Event
+import requests
+from ics import Calendar, Event
 
 
 timeEDIT_URL = "PASTE_YOUR_TIMEEDIT_ICS_URL_HERE"
 OUTPUT_FILE = "docs/clean.ics"  ## file we save new calander in?
 
-raw = request.get(timeEDIT_URL,timeout=30)
+raw = requests.get(timeEDIT_URL,timeout=30)
 raw.raise_for_status()
 
-source = Calender(raw.text)
-clean = Calander()
+source = Calendar(raw.text)
+clean = Calandar()
 
 for i in source.event:
   parts = [p.strip() for p in i.name.split("|")]
+  
   title = part[0]
   details = "\n".join(parts[1:])
+  
   new = Event()
   new.name = title
   new.begin = i.begin
   new.end = i.end
   new.location i.location
   new.description = details
+  
   clean.events.add(new)
+  
 with open(OUTPUT_FILE, "w", encoding = "utf-8") as f:
   f.writelines(clean.serialize())
 
