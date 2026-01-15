4cf442098d4e5c4ee6a2843cc87f8b505dc56bc5
QuTonYa <103885599+QuTonYa@users.noreply.github.com>
2026-01-15 16:14:48 +0100

    Update clean_timeedit.py

--- clean_timeedit.py
+++ clean_timeedit.py
@@ -2,10 +2,10 @@ import request;
 from ics import Calender, Event:
 
 
-TimeEdit_URL = https://cloud.timeedit.net/usn/web/publikk/ri6Q28467l804WQQQWZq566Z7VyY051V9924XlY5W556XZW52612WZ7QQ713nqdnZuo00nm6nwonZl0Q3085Q6l7ZQ8kb9o13tZ889t3B43634AE29FF1DE65A663E319E8C99.ics ## paste your TimeEdit link here
-NEW_TimeEdit_file = "output/clean.ics"  ## file we save new calander in?
+timeEDIT_URL = "PASTE_YOUR_TIMEEDIT_ICS_URL_HERE"
+OUTPUT_FILE = "output/clean.ics"  ## file we save new calander in?
 
-raw = request.get(TimeEdit_URL,timeout=30)
+raw = request.get(timeEDIT_URL,timeout=30)
 raw.raise_for_status()
 
 source = Calender(raw.text)
@@ -22,7 +22,7 @@ for i in source.event:
   new.location i.location
   new.description = details
   clean.events.add(new)
-with open(NEW_TimeEdit_file, "w", encoding = "utf-8") as f:
+with open(OUTPUT_FILE, "w", encoding = "utf-8") as f:
   f.writelines(clean.serialize())
 
 
